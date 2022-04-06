46c6f9609f2b15269ad56181dad4cc31595fbc18
Stefan Minkov <50842434+aRowOf5@users.noreply.github.com>
2022-04-06 21:57:38 +0300

    Add files via upload

---
+++ Event-Time-Date/Date.h
@@ -0,0 +1,202 @@
+﻿#pragma once
+
+enum dayOfWeek {sunday, monday, tuesday, wednesday, thursday, friday, saturday};
+
+int days[] = { 31, 28, 31, 30, 31, 30,
+			   31, 31, 30, 31, 30, 31 };
+
+class Date {
+private:
+	int day, month, year, daysOfMonth, dayOfWeek, dayCount;
+	const int firstDate, firstDayOfWeek, firstYear;
+
+public:
+	Date() : firstDate(1), firstDayOfWeek(saturday), firstYear(1900) {
+		day = 1, month = 1, year = 1900, daysOfMonth = 31, dayOfWeek = saturday, dayCount = 0;
+	}
+	void getNewDate(int day, int month, int year);
+	void getDayOfTheWeek();
+	void tomorrowDate();
+	void getRightDate();
+	void printDate();
+	bool isLeapYear();
+	int dayOfYear();
+};
+
+bool Date::isLeapYear() {
+	if (this->year % 4 == 0) {
+		if (this->year % 100 == 0) { return (this->year % 400 == 0); }
+		return true;
+	}
+	return false;
+}
+
+void Date::getRightDate() {
+	switch (this->month) {
+	case 1: 
+		this->daysOfMonth = 31;
+		break;
+	case 2:
+		if (isLeapYear()) { this->daysOfMonth = 29; break; }
+		this->daysOfMonth = 28;
+		break;
+	case 3:
+		this->daysOfMonth = 31;
+		break;
+	case 4:
+		this->daysOfMonth = 30;
+		break;
+	case 5:
+		this->daysOfMonth = 31;
+		break;
+	case 6:
+		this->daysOfMonth = 30;
+		break;
+	case 7:
+		this->daysOfMonth = 31;
+		break;
+	case 8:
+		this->daysOfMonth = 31;
+		break;
+	case 9:
+		this->daysOfMonth = 30;
+		break;
+	case 10:
+		this->daysOfMonth = 31;
+		break;
+	case 11:
+		this->daysOfMonth = 30;
+		break;
+	case 12:
+		this->daysOfMonth = 31;
+		break;
+	default:
+		break;
+	}
+}
+
+void Date::getNewDate(int day, int month, int year) {
+	this->day = day;
+	this->month = month;
+	this->year = year;
+
+	if (this->day > daysOfMonth) {
+		this->month = this->month + this->day / daysOfMonth;
+		this->day = this->day % this->daysOfMonth;
+	}
+
+	if (this->month > 12) {
+		this->year = this->year + this->month / 12;
+		this->month = this->month % 12;
+	}
+
+	if (this->year == 1916 && this->month == 4) { //смяна на календара
+		if (this->day >= 1 && this->day <= 13) {
+			this->day = this->day + 13;
+		}
+	}
+}
+
+void Date::tomorrowDate() {
+	this->day++;
+	if (this->day > daysOfMonth) {
+		this->month++;
+		this->day = this->day % daysOfMonth;
+	}
+	if (this->month > 12) {
+		this->year++;
+		this->month = this->month % 12;
+	}
+}
+
+int Date::dayOfYear() {
+	int tempYear = this->year;
+	int tempMonth = this->month;
+	dayCount = this->day;
+
+	if (tempMonth > 2 && tempYear % 4 == 0
+		&& (tempYear % 100 != 0 || tempYear % 400 == 0)) {
+		++dayCount;
+	}
+
+	while (tempMonth-- > 0) {
+		dayCount = dayCount + days[tempMonth - 1];
+	}
+	return dayCount;
+}
+
+void Date::getDayOfTheWeek() {
+	dayOfWeek = ((this->year - firstYear) * 365 + (this->year - firstYear) / 4 + dayOfYear()) % 7;
+}
+
+void Date::printDate() {
+	switch (dayOfWeek) { //switch case за ден от седмицата
+	case sunday:
+		std::cout << "Sunday, ";
+		break;
+	case monday:
+		std::cout << "Monday, ";
+		break;
+	case tuesday:
+		std::cout << "Tuesday, ";
+		break;
+	case wednesday:
+		std::cout << "Wednesday, ";
+		break;
+	case thursday:
+		std::cout << "Thursday, ";
+		break;
+	case friday:
+		std::cout << "Friday, ";
+		break;
+	case saturday:
+		std::cout << "Saturday, ";
+		break;
+	default:
+		std::cout << "Error day of the week!";
+		break;
+	}
+
+	switch (month) { //switch case за дата
+	case 1:
+		std::cout << "January " << day << ", " << year << std::endl;
+		break;
+	case 2:
+		std::cout << "February " << day << ", " << year << std::endl;
+		break;
+	case 3:
+		std::cout << "March " << day << ", " << year << std::endl;
+		break;
+	case 4:
+		std::cout << "April " << day << ", " << year << std::endl;
+		break;
+	case 5:
+		std::cout << "May " << day << ", " << year << std::endl;
+		break;
+	case 6:
+		std::cout << "June " << day << ", " << year << std::endl;
+		break;
+	case 7:
+		std::cout << "July " << day << ", " << year << std::endl;
+		break;
+	case 8:
+		std::cout << "August " << day << ", " << year << std::endl;
+		break;
+	case 9:
+		std::cout << "September " << day << ", " << year << std::endl;
+		break;
+	case 10:
+		std::cout << "October " << day << ", " << year << std::endl;
+		break;
+	case 11:
+		std::cout << "November " << day << ", " << year << std::endl;
+		break;
+	case 12:
+		std::cout << "December " << day << ", " << year << std::endl;
+		break;
+	default:
+		std::cout << "Error month!";
+		break;
+	}
+	
+}
---
+++ Event-Time-Date/Time.cpp
@@ -0,0 +1,135 @@
+#include <iostream>
+#include "Date.h"
+
+class Time {
+private:
+    int hours, minutes, seconds;
+
+public:
+    Time() {
+        hours = 0, minutes = 0, seconds = 0;
+    }
+    int compare(int hours, int minutes, int seconds);
+    void getToMidnight() const;
+    bool isDinnerTime();
+    bool isPartyTime();
+    void print(bool is12HoursClock);
+    void getNewTime(int hours, int minutes, int seconds);
+};
+
+void Time::getNewTime(int hours, int minutes, int seconds) {
+    this->hours = hours;
+    this->minutes = minutes;
+    this->seconds = seconds;
+}
+
+int Time::compare(int hours, int minutes, int seconds) {
+    if (hours < this->hours) {
+        if (minutes < this->minutes) {
+            if (seconds < this->seconds) {
+                Time::getNewTime(hours, minutes, seconds);
+                return 1;
+            }
+            else if(seconds > this->seconds) {
+                Time::getNewTime(hours, minutes, seconds);
+                return -1;
+            }
+        }
+        else if (minutes > this->minutes) {
+            Time::getNewTime(hours, minutes, seconds);
+            return -1;
+        }
+    }
+    else if (hours > this->hours) {
+        Time::getNewTime(hours, minutes, seconds);
+        return -1;
+    }
+    Time::getNewTime(hours, minutes, seconds);
+    return 0;
+}
+
+void Time::getToMidnight() const{
+    int midNightH = 23 - hours, midNightM = 59 - minutes, midNightS = 60 - seconds;
+
+    if (midNightH < 10) { std::cout << "Time until midnight: " << "0" << midNightH << ":"; }
+    else { std::cout << "Time until midnight: " << midNightH << ":"; }
+
+    if (midNightM < 10) { std::cout << "0" << midNightM << ":"; }
+    else { std::cout << midNightM << ":"; }
+
+    if (midNightS < 10) { std::cout << "0" << midNightS << std::endl; }
+    else { std::cout << midNightS << std::endl; }
+    //std::cout << "Time until midnight: " << midNightH << ":" << midNightM << ":" << midNightS << std::endl;
+}
+
+bool Time::isDinnerTime() {
+    if (hours >= 20 && hours <= 22) {
+        if ((hours == 20 && minutes >= 30) || (hours == 22 && minutes == 0)) {
+            return true;
+        }
+        return true;
+    }
+    return false;
+}
+
+bool Time::isPartyTime() {
+    if (hours == 23 || (hours >= 0 && hours <= 6)) {
+        if ((hours == 23 || hours == 6) && minutes == 0) {
+            return true;
+        }
+        return true;
+    }
+    return false;
+}
+
+void Time::print(bool is12HoursClock) {
+    is12HoursClock = false;
+    int tempAns = 0;
+    std::cout << "How many hours does your clock have?";
+    std::cin >> tempAns;
+    if (tempAns == 12) {
+        is12HoursClock = true;
+    }
+    else if(tempAns != 24) {
+        std::cout << "This clock doesn't exist!" << std::endl;
+        return;
+    }
+
+    if (is12HoursClock) {
+        if (hours - 12 < 0) {
+            if (hours < 10) { std::cout << "The time is " << "0" << hours << ":"; }
+            else { std::cout << "The time is " << hours << ":"; }
+
+            if (minutes < 10) { std::cout << "0" << minutes << ":"; }
+            else { std::cout << minutes << ":"; }
+
+            if (seconds < 10) { std::cout << "0" << seconds << "AM." << std::endl; }
+            else { std::cout << seconds << std::endl; }
+        }
+        else {
+            if (hours - 12< 10) { std::cout << "The time is " << "0" << hours << ":"; }
+            else { std::cout << "The time is " << hours << ":"; }
+
+            if (minutes < 10) { std::cout << "0" << minutes << ":"; }
+            else { std::cout << minutes << ":"; }
+
+            if (seconds < 10) { std::cout << "0" << seconds << "PM." << std::endl; }
+            else { std::cout << seconds << std::endl; }
+        }
+    }
+    else {
+        if (hours < 10) { std::cout << "The time is " << "0" << hours << ":"; }
+        else { std::cout << "The time is " << hours << ":"; }
+
+        if (minutes < 10) { std::cout << "0" << minutes << ":"; }
+        else { std::cout << minutes << ":"; }
+
+        if (seconds < 10) { std::cout << "0" << seconds << std::endl; }
+        else { std::cout << seconds << std::endl; }
+    }
+}
+
+
+int main() {
+    return 0;
+}
